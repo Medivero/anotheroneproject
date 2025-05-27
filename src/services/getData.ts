5b5a1b2af938734870bdb42124c71e3631e79e81
Medivero <artembolyachinov@gmail.com>
2025-05-27 17:20:08 +0300

    tests

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,9 +1,16 @@
 import { ApiURL } from "../store/api";
 
 export async function getData() {
+    
     const res = await fetch(ApiURL+"posts",{
         method: "GET"
     })
-    const data = await res.json()
-    return data;
+    try{
+        const data = await res.json()
+        await console.log(data)
+        return data;
+    }
+    catch(error){
+        console.log(error)
+    }
 }
--- src/services/postData.ts
+++ src/services/postData.ts
@@ -1,8 +1,15 @@
 import { ApiURL } from "../store/api";
 
 export default function postData(formObj:any[]) {
-    fetch(ApiURL+"posts",{
-        method: "POST",
-        body: JSON.stringify(formObj)
-    })
+    try{
+        fetch(ApiURL+"posts",{
+            method: "POST",
+            body: JSON.stringify(formObj)
+        }).then(() => {
+            console.log(formObj);
+            console.log("Успешно добавлено!")
+        })
+    } catch(error){
+        console.log(error)
+    }
 }
